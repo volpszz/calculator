784f0031cd8bdb22840c1cd6f7d2be83aa5123c7
volpsz <76572284+volpszz@users.noreply.github.com>
2024-07-09 11:09:42 -0300

    Add files via upload

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,6 @@
 # operações basicas
 
-
+import math
 
 def adicao (a,b):
     return a + b
@@ -9,8 +9,6 @@ def subtracao (a,b):
     return a - b
 
 def multiplicacao (a,b):
-    if b == 0:
-        return "Todo numero multiplicado por 0 é 0"
     return a * b
 
 def divisao (a,b):
@@ -18,40 +16,81 @@ def divisao (a,b):
         return "Não é possivel fazer divisão por zero."
     return a / b
 
+def potencia (a,b):
+    return a ** b
+
+def raiz (a):
+    return math.sqrt(a)
+    
+       
+ 
+    
+
 print("Escolha qual operação:")
 print("1.adiçao")
 print("2.subtração")
 print("3.multiplicação")
 print("4.divisão")
+print("5.potencia")    
+print("6.raiz quadrada")   
 
 while True:
 
 
-    escolha = input("Escolha a opção (1/2/3/4:) ")
-    if escolha in ( "1", "2", "3", "4"):
+    escolha = input("Escolha a opção (1/2/3/4/5/6:) ")
+    if escolha in ( "1", "2", "3", "4","5",):        
         a = float(input("Digite o primeiro numero: "))    
         b = float(input("Digite o segundo numero: "))
 
-     
-        if escolha == '1':
+
+
+    if escolha in ("6"):
+        ab = float(input("Digite o número da raiz: "))
+
+    
+
+    
+
+
+    if escolha == '1':
    
             print(adicao(a,b))
 
-        elif escolha == '2':
+    elif escolha == '2':
    
              print(subtracao(a,b)) 
               
-        elif escolha == '3':
+    elif escolha == '3':
    
              print(multiplicacao(a,b))
 
-        elif escolha == '4':
+    elif escolha == '4':
    
              print(divisao(a,b))
 
-             break      
+    elif escolha == '5':
+            print(potencia(a,b)) 
+
+    elif escolha == '6':
+             print(f"√{ab} = {raiz(ab)}")
+
+
+
+
+    else:
+         print("Opção inválida, Digite um número de 1 a 6.")
+         continue        
+
+    nova_operação = input("Deseja fazer uma nova operação? (S/N): ")
+    if nova_operação.lower() != 's':
+        break
+
+
+
+
+    print("programa encerrado.")
+    
+
         
-                                                          
 
-
-
+            
